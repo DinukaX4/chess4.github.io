806ce1772a9e6f4e8467484e350c8cb3389891de
DinukaX4 <31525330+DinukaX4@users.noreply.github.com>
2017-09-05 12:07:48 +0530

    chesss

--- Js/HomeControler.js
+++ Js/HomeControler.js
@@ -1,6 +1,32 @@
 $("img").addClass("img-responsive");
 $("img").draggable({revert: 'invalid'});
-$(function () {
 
-})
 $("#Brow").droppable();
+var BP1=0;
+console.log(BP1);
+var bp1=$("#BP-1").position();
+console.log(bp1.top);
+
+var intbp1=0;
+
+
+$("#BP-1").mouseover(function (evt) {
+    if(bp1.top===20){
+        intbp1=intbp1+0;
+        console.log(intbp1);
+        $("#D1").css("background-color","red");
+        $("#C1").css("background-color","red");
+        $("#C1").droppable();
+        $("#D1").droppable();
+        bp1=$("#BP-1").position();
+        
+    }else{
+        $("#C1").css("background-color","#1b6d85");
+        $("#D1").css("background-color","black");
+    }
+    //$("#C1").css("background-color","red")
+});
+$("#BP-1").mouseleave(function (evt) {
+    $("#C1").css("background-color","#1b6d85");
+    $("#D1").css("background-color","black");
+})
